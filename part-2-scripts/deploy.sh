7739188b4245e3261a9e40953f8a584d04752a24
shashwa7-dev <shashwa7.dev@gmail.com>
2023-09-10 17:52:27 +0530

    app title updated!

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.6.1/bin
 
 cd week-9
  git pull origin master
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello Course App!"));
+app.get("/", (req, res) => res.send("Hello Course App! V2"));
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 
